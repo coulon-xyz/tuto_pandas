5be48f656d07947f201a4e668ed1e0d3869ea496
Coulon <julien@coulon.xyz>
2017-12-21 16:23:00 +0100

    remove build df function

--- get_data.py
+++ get_data.py
@@ -22,23 +22,10 @@ def download_prices(acronyms):
         out_file = acronym + ".csv"
         p =download_file(build_url(url_root, acronym), out_file)
         print("{} was download in {}".format(acronym, p))
-def build_df(acronyms):
-    l_df = list()
-    for acronym in acronyms:
-        file_to_load = os.path.join(OUTPUT, acronym + ".csv")
-        print("-- {} --".format(acronym))
-        df = pd.read_csv(file_to_load, index_col=False, usecols=['date', 'price(USD)'])
-        df = df.set_index("date")
-        df = df.rename(columns={'price(USD)': acronym})
-        l_df.append(df)
-        del df
-
-    return pd.concat(l_df, axis=1)
 
 
 if __name__ == "__main__":
     acronyms = ['eth', 'btc', 'bch', 'ltc', 'xem', 'dcr', 'zec', 'dash', 'doge', 'etc', 'pivx', 'xmr', 'vtc']
     download_prices(acronyms)
-    # df = build_df(acronyms)
 
 
