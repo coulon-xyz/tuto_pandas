18f128570cf1860644d4ee0e98240eb5321fbac5
Coulon <julien@coulon.xyz>
2017-12-21 16:27:30 +0100

    added constants.py

---
+++ constants.py
@@ -0,0 +1,8 @@
+import os
+
+ABSOLUTE = os.path.join(os.path.dirname(__file__))
+OUTPUT = os.path.join(ABSOLUTE, "runtime")
+
+api_url_root = 'https://coinmetrics.io/data/'
+
+acronyms = ['eth', 'btc', 'bch', 'ltc', 'xem', 'dcr', 'zec', 'dash', 'doge', 'etc', 'pivx', 'xmr', 'vtc']
--- get_data.py
+++ get_data.py
@@ -1,29 +1,27 @@
 import urllib.request
 import os
+import constants
 
-ABSOLUTE = os.path.join(os.path.dirname(__file__))
-OUTPUT = os.path.join(ABSOLUTE, "runtime")
-
-url_root = 'https://coinmetrics.io/data/'
 
 
 def download_file(url, file_name):
-    output_path = os.path.join(OUTPUT, file_name)
+    output_path = os.path.join(constants.OUTPUT, file_name)
     urllib.request.urlretrieve(url, output_path)
     return output_path
+
 
 def build_url(url, acronym):
     return url + acronym + ".csv"
 
+
 def download_prices(acronyms):
     for acronym in acronyms:
         out_file = acronym + ".csv"
-        p =download_file(build_url(url_root, acronym), out_file)
+        p =download_file(build_url(constants.api_url_root, acronym), out_file)
         print("{} was download in {}".format(acronym, p))
 
 
 if __name__ == "__main__":
-    acronyms = ['eth', 'btc', 'bch', 'ltc', 'xem', 'dcr', 'zec', 'dash', 'doge', 'etc', 'pivx', 'xmr', 'vtc']
-    download_prices(acronyms)
+    download_prices(constants.acronyms)
 
 
